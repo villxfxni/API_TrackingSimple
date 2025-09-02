25c756e99b126aeeb628a11a1419a7188507356d
villxfxni <anagvillafanis@gmail.com>
2025-09-01 23:28:52 -0400

    Commit Inicial, 4 controllers creados con sample UI para CRUD de Usuarios

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,10 +9,12 @@ use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
+    web: __DIR__.'/../routes/web.php',
+    api: __DIR__.'/../routes/api.php',      // ← THIS MUST BE PRESENT
+    commands: __DIR__.'/../routes/console.php',
+    health: __DIR__.'/../routes/health.php',
+)
+
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
